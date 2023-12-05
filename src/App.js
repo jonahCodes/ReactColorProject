cc908157e17d7fcbbfe5f5b8ee561cce25be9809
john developer <51842159+jonahCodes@users.noreply.github.com>
2023-12-05 17:57:04 +0000

    add palette react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import "./styles.css";
 //test commit
+import { Component } from "react";
 import Palette from "./colorApp/Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorApp/colorHelper";
+import { Route, Switch } from "react-router-dom";
 
-export default function App() {
-  return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
-  );
+export default class App extends Component {
+  findPalette = (id) => {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  };
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>hello home</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id),
+              )}
+            />
+          )}
+        />
+      </Switch>
+    );
+  }
 }
