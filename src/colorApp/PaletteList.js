d40002182c03af20ede923c621d1e41fa5103698
john developer <51842159+jonahCodes@users.noreply.github.com>
2023-12-07 20:14:34 +0000

    add mini palette styles class

--- src/colorApp/PaletteList.js
+++ src/colorApp/PaletteList.js
@@ -1,11 +1,20 @@
 import { Component } from "react";
 // import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
+const classes = {
+  root: {
+    backgroundColor: "blue",
+  },
+};
 export default class PaletteList extends Component {
   render() {
     const { palettes } = this.props;
     return (
-      <div>
+      <div style={classes.root}>
+        <div style={classes.container}>
+          <nav style={classes.nav}></nav>
+          <div style={classes.palette}></div>
+        </div>
         <h1>hello</h1>
         {palettes.map((palette, index) => (
           <p key={index}>
