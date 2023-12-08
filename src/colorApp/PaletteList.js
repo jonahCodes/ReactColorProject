6578467d2c47a5122a99cd5de4b2a14eab3e43e0
john developer <51842159+jonahCodes@users.noreply.github.com>
2023-12-08 16:25:33 +0000

    start palettelist styling

--- src/colorApp/PaletteList.js
+++ src/colorApp/PaletteList.js
@@ -1,27 +1,56 @@
+import { withStyles } from "react-jss";
 import { Component } from "react";
 // import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
-const classes = {
+
+const styles = {
   root: {
     backgroundColor: "blue",
+    height: "100%",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    flexWrap: "wrap",
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    color: "white",
+  },
+  palettes: {
+    display: "grid",
+    boxSizing: "border-box",
+    width: "100%",
+    gridTemplateColumns: "repeat(3,30%)",
+    gridGap: "5%",
   },
 };
-export default class PaletteList extends Component {
+class PaletteList extends Component {
   render() {
-    const { palettes } = this.props;
+    const { palettes, classes } = this.props;
     return (
-      <div style={classes.root}>
-        <div style={classes.container}>
-          <nav style={classes.nav}></nav>
-          <div style={classes.palette}></div>
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1>React ColorApp</h1>
+          </nav>
+          <div className={classes.palettes}>
+            {palettes.map((palette, index) => (
+              <p key={index}>
+                <MiniPalette {...palette} />
+              </p>
+            ))}
+          </div>
         </div>
-        <h1>hello</h1>
-        {palettes.map((palette, index) => (
-          <p key={index}>
-            <MiniPalette {...palette} />
-          </p>
-        ))}
       </div>
     );
   }
 }
+export default withStyles(styles)(PaletteList);
